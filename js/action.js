74dc09b3672fe571df45a997d96d63db3e841b7c
Mendelev Vladimir <qutrn125n@yandex.ru>
2023-03-16 09:44:36 +0300

    Добавил кнопку вверх

--- js/action.js
+++ js/action.js
@@ -132,5 +132,38 @@ document.addEventListener('keydown', function (event) {
 });
 // modal window end
 
+// btn up start
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    show() {
+        // удалим у кнопки класс btn-up_hide
+        this.el.classList.remove('btn-up-hide');
+    },
+    hide() {
+        // добавим к кнопке класс btn-up-hide
+        this.el.classList.add('btn-up-hide');
+    },
+    addEventListener() {
+        // при прокрутке содержимого страницы
+        window.addEventListener('scroll', () => {
+            // определяем величину прокрутки
+            const scrollY = window.scrollY || document.documentElement.scrollTop;
+            // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+            scrollY > 400 ? this.show() : this.hide();
+        });
+        // при нажатии на кнопку .btn-up
+        document.querySelector('.btn-up').onclick = () => {
+            // переместим в начало страницы
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            });
+        }
+    }
+}
+
+btnUp.addEventListener();
+// btn up end
 
 // })();
